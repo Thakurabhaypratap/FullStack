854f1004db6e3e8bded1cd38dfa3e864430ade80
Abhay <zzxxcc6386@gmail.com>
2024-10-15 01:45:13 +0530

    f

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios from 'axios';
+import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [jokes, setJokes] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/jokes')
+      .then(response => {
+        setJokes(response.data);
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      }); // Fetch data from the API and log it to the console
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Jokes</h1>
+      <p>Number of jokes: {jokes.length}</p>
+
+      {jokes.map(req => (
+        <div key={req.id}>
+          <p>ID: {req.id}</p>
+          <p>Joke: {req.joke}</p>
+        </div>
+      ))}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
